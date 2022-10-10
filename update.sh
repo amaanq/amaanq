ae96c1c891519c1e68ce14cabe919829adf2ac7b
Amaan Qureshi <amaanq12@gmail.com>
2022-10-10 02:58:49 -0400

    without scheck fixes

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -e
 
 username="amaanq"
@@ -7,41 +6,40 @@ in="README.tmpl.md"
 out="README.md"
 
 main() {
-	# Dependencies
+	# Dependencies.
 	require curl jq sort uniq
 
 	# Require the metrics token for API querying.
-	[[ "$METRICS_TOKEN" ]] || fatal "missing $METRICS_TOKEN"
+	[[ "$METRICS_TOKEN" ]] || fatal 'missing $METRICS_TOKEN'
 
 	reposJSON=()
 	reposPage=0
-
-	for (( ; ; )); do
+	for ((;;)); {
 		last=1
 		while read -r json; do
-			reposJSON+=("$json")
+			reposJSON+=( "$json" )
 			last=0
 		done < <(queryGitHub "/user/repos?per_page=100&page=$reposPage&type=public" | jq -c '.[]')
 
 		# Out of pages. Break.
-		((last == 1)) && break
+		(( last == 1 )) && break
 		# Not out of pages. Continue.
-		((reposPage++)) && continue
-	done
+		(( reposPage++ )) && continue
+	}
 
 	publicReposJSON=$(printf "%s\n" "${reposJSON[@]}" | jq -c "select((.private | not ) and (.fork | not))")
 
-	render >"$out"
+	render > "$out"
 	saveCSV "data/stargazers.csv" "$(nStargazers)"
 
 	# This does virtually nothing, but hopefully we can improve it to be better
 	# in the future. For now, just remove the faulty record manually.
-	movingAverage "data/stargazers.csv" 4 |
-		renderSVG "-7 days" gold \
-			>sparklines/stargazers.svg
+	movingAverage "data/stargazers.csv" 4 \
+		| renderSVG "-7 days" gold        \
+		> sparklines/stargazers.svg
 }
 
-# saveCSV <filepath> <value>
+# saveCSV filepath value
 # saveCSV saves the given value into the file but only if the value differs.
 saveCSV() {
 	local filepath="$1"
@@ -50,7 +48,7 @@ saveCSV() {
 
 	{
 		# Get the last line of the CSV file.
-		record="$(tail -n1 "$filepath" 2>/dev/null)"
+		record="$(tail -n1 "$filepath" 2> /dev/null)"
 		# Get the last column, which is the value, and compare it with the
 		# current value. Succeed if the value is the same.
 		[[ "${record##*,}" == "$value" ]]
@@ -59,7 +57,7 @@ saveCSV() {
 		mkdir -p "$(dirname "$filepath")"
 		# Value is not the same or the value doesn't exist. Add the record.
 		printf -v ts "%(%s)T"
-		printf "%d,%d\n" "$ts" "$value" >>"$filepath"
+		printf "%d,%d\n" "$ts" "$value" >> "$filepath"
 	}
 }
 
@@ -68,39 +66,39 @@ movingAverage() {
 	local path="$1"
 	local n="$2"
 
-	readarray -d $'\n' rows <"$path"
+	readarray -d $'\n' rows < "$path"
 
 	# Don't process if we have less records than the size of the window.
-	((${#rows[@]} < n)) && {
+	(( ${#rows[@]} < n )) && {
 		printf "%s\n" "${rows[@]}"
 		return
 	}
 
 	window=()
-	for row in "${rows[@]}"; do
-		IFS=, values=("$row")
+	for row in "${rows[@]}"; {
+		IFS=, values=( $row )
 		local t="${values[0]}"
 		local v="${values[1]}"
 
-		window+=("$v")
+		window+=( "$v" )
 
-		((${#window[@]} <= n)) && {
+		(( ${#window[@]} <= n )) && {
 			continue
 		}
 
 		# Pop off the first entry.
-		window=("${window[@]:1}")
+		window=( "${window[@]:1}" )
 
 		# Average out the window.
 		local avg=0
-		for value in "${window[@]}"; do
-			avg=$((avg + value))
-		done
-		avg=$((avg / ${#window[@]}))
+		for value in "${window[@]}"; {
+			avg=$[ avg + value ]
+		}
+		avg=$[ avg / ${#window[@]} ]
 
 		# Print record
 		echo "$t,$avg"
-	done
+	}
 }
 
 # renderSVG "date" "color" < stdin
@@ -114,10 +112,10 @@ renderSVG() {
 	local h=10
 	local m=100 # multiples for accuracy; beware of epoch overflow
 
-	w=$((w * m))
-	h=$((h * m))
+	w=$[ w * m ]
+	h=$[ h * m ]
 
-	cat <<EOL
+	cat << EOL
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 $w $h">
 EOL
 
@@ -132,48 +130,48 @@ EOL
 	endAt=$(echo -n "${rows[-1]}" | cut -d, -f1)
 	startAt=$(date -d "$(date -d "@$endAt") -7 days" +%s)
 
-	for row in "${rows[@]}"; do
+	for row in "${rows[@]}"; {
 		# Parse the row into an array of values delimited by a comma.
-		IFS=, values=("$row")
+		IFS=, values=( $row )
 		local t="${values[0]}"
 		local v="${values[1]}"
 
 		# If the datapoint is outside the time range, then skip it.
-		((t < startAt)) && continue
+		(( t < startAt )) && continue
 
-		((v > max)) || [[ ! "$max" ]] && max=$v
-		((v < min)) || [[ ! "$min" ]] && min=$v
-	done
+		(( v > max )) || [[ ! "$max" ]] && max=$v
+		(( v < min )) || [[ ! "$min" ]] && min=$v
+	}
 
 	# Deal with the inaccurate integer math by scaling up the values.
-	min=$((min * m))
-	max=$((max * m))
+	min=$[ min * m ]
+	max=$[ max * m ]
 	# Add a bit of headroom (5% or 20/100 parts extra) for the max value.
-	max=$((max + (max / (20 * m))))
+	max=$[ max + (max / (20 * m)) ]
 	# Do a bit more (10% or 10/100) for the min value.
-	min=$((min - (min / (10 * m))))
+	min=$[ min - (min / (10 * m)) ]
 	# gap is the difference between the minimum and maximum value.
-	gap=$((max - min))
+	gap=$[ max - min ]
 
 	printf '\t'
 	printf '<path fill="none" stroke="%s" stroke-width="125" stroke-linecap="round" stroke-linejoin="round" d="' "$color"
 
 	local drew=
 
-	for row in "${rows[@]}"; do
-		IFS=, values=("$row")
+	for row in "${rows[@]}"; {
+		IFS=, values=( $row )
 		local t="${values[0]}"
 		local v="${values[1]}"
 
 		# Calculate the time instant if the startAt were to be the starting
 		# point.
-		t=$((t - startAt))
+		t=$[ t - startAt ]
 
 		# Scale the value up.
-		v=$((v * m))
+		v=$[ v * m ]
 
-		x=$((t * w / (endAt - startAt)))
-		y=$(((gap - (v - min)) * h / gap))
+		x=$[ t * w / (endAt - startAt) ]
+		y=$[ (gap - (v - min)) * h / gap ]
 
 		# Draw the first move command if we haven't.
 		[[ ! $drew ]] && {
@@ -182,7 +180,7 @@ EOL
 		}
 
 		printf ' L%d %d' "$x" "$y"
-	done
+	}
 
 	echo '" />'
 	echo '</svg>'
@@ -200,24 +198,24 @@ nStargazers() {
 
 # repoLanguages ($publicReposJSON)
 repoLanguages() {
-	echo -n "$publicReposJSON" |
-		jq -rn 'inputs.language | select(. != null)' |
-		sort | uniq -c | sort -nr |
-		topUnique 3
+	echo -n "$publicReposJSON"                         \
+		| jq -rn 'inputs.language | select(. != null)' \
+		| sort | uniq -c | sort -nr                    \
+		| topUnique 3
 }
 
 # repoLicenses ($publicReposJSON)
 repoLicenses() {
-	echo -n "$publicReposJSON" |
-		jq -rn 'inputs.license.spdx_id | select(. != null)' |
-		sort | uniq -c | sort -nr |
-		topUnique 3
+	echo -n "$publicReposJSON"                             \
+		| jq -rn 'inputs.license.spdx_id | select(. != null)' \
+		| sort | uniq -c | sort -nr                        \
+		| topUnique 3
 }
 
 # topUnique numUnique < uniqOutput
 topUnique() {
 	numUnique="${1:- 0}"
-	((numUnique < 1)) && return 1
+	(( numUnique < 1 )) && return 1
 
 	readarray -d $'\n' lines
 
@@ -225,34 +223,33 @@ topUnique() {
 	top=()
 	sum=0
 
-	for line in "${lines[@]}"; do
+	for line in "${lines[@]}"; {
 		[[ "$line" =~ \ *([0-9]+)\ ([A-Za-z0-9 ]+) ]] && {
 			local count=${BASH_REMATCH[1]}
 			local item=${BASH_REMATCH[2]}
 
-			local percentage=$((count * 100 / nrepos))
-			top+=("$item ($percentage%)")
+			local percentage=$[ count * 100 / nrepos ]
+			top+=( "$item ($percentage%)" )
 
-			((sum += percentage)) || true # (()) is weird.
-			((${#top[@]} == numUnique)) && break
+			(( sum += percentage )) || true # (()) is weird.
+			(( ${#top[@]} == numUnique )) && break
 		}
-	done
+	}
 
-	((sum == 0)) && return 0
+	(( sum == 0 )) && return 0
 
 	printf "%s, " "${top[@]}"
-	echo -n "and others ($((100 - sum))%)"
+	echo -n "and others ($[100-sum]%)"
 }
 
 render() {
-	local render
-	render="$(mktemp)"
+	local render="$(mktemp)"
 
 	(
 		echo "cat << __EOF__"
 		cat "$in"
 		echo "__EOF__"
-	) >"$render"
+	) > "$render"
 
 	. "$render"
 
@@ -265,17 +262,17 @@ fatal() {
 }
 
 require() {
-	for dep in "$@"; do
-		command -v &>/dev/null || fatal "missing $dep"
-	done
+	for dep in "$@"; {
+		command -v &> /dev/null || fatal "missing $dep"
+	}
 }
 
-# GET GitHub path...
+# GETGitHub path...
 # queryGitHub GETs a GitHub REST API path.
 queryGitHub() {
 	httpGET \
 		-H "Accept: application/vnd.github.v3+json" \
-		-H "Authorization: token $METRICS_TOKEN" \
+		-H "Authorization: token $METRICS_TOKEN"    \
 		"https://api.github.com$1"
 }
 
